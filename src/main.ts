2445fd32e86b8d76fe6d5331aacdcb40b3ba45dc
Federico Mameli <federico_mameli@yahoo.com>
2024-04-12 12:35:18 +0000

    feat: setup ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
@@ -6,6 +7,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  app.useGlobalPipes(new ValidationPipe({ exceptionFactory }));
   await app.listen(configService.get('APP_PORT'));
 }
 bootstrap();
+
+export function exceptionFactory(validationErrors) {
+  const errors = validationErrors.reduce((errors, error) => {
+    errors[error.property] = Object.values(error.constraints);
+
+    return errors;
+  }, {});
+
+  return new BadRequestException({ errors });
+}
